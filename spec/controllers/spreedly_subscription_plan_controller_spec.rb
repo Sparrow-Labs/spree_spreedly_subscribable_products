5449ef23f0f5299b7bfdfcc5e483f21edb2af887
ji <yovoslav.ivanov@gmail.com>
2013-03-09 22:22:27 +0100

    Completes integration tests for subscription plan controller.

--- spec/controllers/spreedly_subscription_plan_controller_spec.rb
+++ spec/controllers/spreedly_subscription_plan_controller_spec.rb
@@ -1,32 +1,54 @@
 require 'spec_helper'
 require 'spree/core/testing_support/factories'
 
-def setup_products_and_feature_levels
-  
+def any_feature_level
+  RSpreedly::SubscriptionPlan.all.first.feature_level
+end
+
+def any_subscription_plan
+  RSpreedly::SubscriptionPlan.all.first
 end
 
 describe SpreedlySubscriptionPlanController do
 
   describe "GET 'index'" do
-    setup_products_and_feature_levels
+    product = FactoryGirl.create :product
 
     it "returns http success" do
-      get 'index', use_route: :spree
+      get 'index', {
+        model_name: 'product',
+        model_id: product.id,
+        feature_level: any_feature_level,
+        use_route: :spree
+      }
+
       response.should be_success
+      product.delete
     end
   end
 
   describe "GET 'show'" do
     it 'returns http success' do
-      get 'show', use_route: :spree
+      get 'show', {
+        use_route: :spree,
+        plan_id: any_subscription_plan.id
+      }
+
       response.should be_success
     end
   end
 
   describe "GET 'feature_levels'" do
+    product = FactoryGirl.create :product
+
     it 'returns http success' do
-      get 'feature_levels', use_route: :spree
+      get 'feature_levels', {
+        use_route: :spree,
+        product_id: product.id
+      }
+
       response.should be_success
+      product.delete
     end
   end
 end
